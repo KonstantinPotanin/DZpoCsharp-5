2f36010c1760f8ce8c7d848b20fa0d51379ff1ed
Konstantin <pkv90@yandex.ru>
2023-02-13 16:00:58 +0500

    Решение второй задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] InitArray ()
+{
+    int [] result = new int [4];
+    Random rnd = new Random ();
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result [i] = rnd.Next(-10, 11);
+    }
+    return result;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array [i]} ");
+    }
+    Console.WriteLine();
+}
+
+int SumOddIndexArray (int [] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr [i];
+    }
+    return sum;
+}
+
+int [] array = InitArray();
+PrintArray(array);
+int sumOddInd = SumOddIndexArray(array);
+Console.WriteLine($"Сумма нечетных индексов в масиве равна {sumOddInd}");
